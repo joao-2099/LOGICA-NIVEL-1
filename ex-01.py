5b617d42a6c5e675453074e315d50408931b26a4
JOÃO PEDRO ROSA <joaopedrorosa546@gmail.com>
2025-03-17 20:46:52 -0300

    Create ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,7 @@
+#10 - problema: escreva um software informe a média aritimética de um aluno que cursou 4 bimestres
+
+nota1 = int (input("Nota do 1º bimestre de Alice: "))
+nota2 = int (input("Nota do 2º bimestre de Alice: "))
+nota3 = int (input("Nota do 3º bimestre de Alice: "))
+nota4 = int (input("Nota do 4º bimestre de Alice: ")) 
+print ("A média aritmética será", (nota1 + nota2 + nota3 + nota4) / 4)
